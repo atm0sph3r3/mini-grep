2028da1b84971c126d0b693f4bc8ba989528383e
tb120 <tb120@users.noreply.github.com>
2018-11-01 15:48:05 -0400

    finish chapter

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
@@ -17,8 +17,7 @@ fn main() {
     // this is a shortcut for defining the return of `run(config)`
     // and then `match`ing on `Err` and ignoring all others
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
-
