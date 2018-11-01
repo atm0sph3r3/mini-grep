e6be4e9e2813c16d07de7ec31b6044a5e59b7a8f
tb120 <tb120@users.noreply.github.com>
2018-10-31 21:45:02 -0400

    move code to lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,27 @@
+extern crate minigrep;
+
 use std::env;
-use std::fs;
 use std::process;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-       println!("Problem parsing arguments: {}", err);
+        println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    run(config);
-}
-
-fn run(config: Config) {
-    let contents = fs::read_to_string(config.filename)
-        .expect("Something went wrong reading the file.");
-
-    println!("With text:\n{}", contents);
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str>{
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
+    // if and only if `run(config)` returns `Err`, enter this scope
+    // this is a shortcut for defining the return of `run(config)`
+    // and then `match`ing on `Err` and ignoring all others
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
     }
 }
+
